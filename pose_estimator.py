4efaf2afb7eea1609f59b24a29a6525d4bd4fab2
Mohamed Khaled <mohamed.k.sharaf@gmail.com>
2023-11-29 20:21:14 +0200

    To Do: 1) Fix the confidence and the number of points 2) Optimize more 3) Add Calibration 4) Add depth map
    What changed?
    1) Optimized the program
    2) Added threading

--- pose_estimator.py
+++ pose_estimator.py
@@ -1,22 +1,25 @@
 import torch
 from ultralytics import YOLO
 import numpy as np
-import queue
-import cv2
 
 # Setting up the pose estimation model
 model = YOLO('YOLOv8s-pose.pt')
 # Set the device to GPU if CUDA is installed and use the model with GPU
 device = 0 if torch.cuda.is_available() else 'cpu'
 print(device)
+
 def getKeypoints(results):
     for result in results:
+        # Extracts the keypoints from the predications
         keypoint = result.keypoints.cpu().numpy()
         if np.shape(keypoint) == ((1, 17, 3)):
+            # Removes the final column because so far we only need x and y
             xy_keypoints = np.delete(keypoint, np.s_[-1:], axis=2)
             return xy_keypoints
 def framePose(id, frame):
+    # This initializes the YOLOv8 model to use it to predict the keypoints in each frame
     results = model.predict(source=frame, conf=0.75, device=device, show_labels=False, show_conf=False, boxes=False, verbose=False)
+    # We make sure that we are sending a value back to the cameras
     if getKeypoints(results) is not None:
         points = getKeypoints(results)
         return points
